bb202b3af0d6c36cda73c566d8429be6e666e37f
Fox <dilvanajavier@gmail.com>
2026-06-24 20:54:05 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,22 @@
 
 
 
-from Menus import *
-Stm_Menus.menu_funcionario() #Roda o loop do primeiro Menu.
+from Menus import Stm_Menus as start
+start.menu_funcionario(None) #Roda o loop do primeiro Menu.
 
+
+
+
+
+
+
+
+
+#===============================================================#
+#       Mensagens de coisas aleatórias que seriam util ler:
+#===============================================================#
+
+#To usando "None" como o return de todos os erros.
+
+#O sistema de checkar se tem ou n gerente ta feito.
+#Vou tentar fazer o sistema de login ainda hoje.
