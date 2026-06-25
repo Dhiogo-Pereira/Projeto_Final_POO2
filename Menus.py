4aeccaf5afb4efeebb967b7108e936b017275b18
Fox <dilvanajavier@gmail.com>
2026-06-25 16:01:18 -0300

    Update Menus.py

--- Menus.py
+++ Menus.py
@@ -4,44 +4,60 @@ from Funcionario import Funcionarios as F
 from Funcionario import Gerente as G
 from Login import login
 
+#====================================================================================
+Juan = G("Juan", "NUMERO", "123Mamaco^^")
+Dhiogo = G("Dhiogo", "NUMERO", "Biruleibe12!")
+
+Joselina = C("Joelina", "NUMERO", [])
+
+# Banco de dados temporario
+c = {"clientes": [Joselina]}
+q = {"quartos": []}
+a = {"alugueis": []}
+f = {"funcionarios": [Juan, Dhiogo]}
+#=====================================================================================
+# Informação do banco de dados
+#=====================================================================================
+
+def SalvaBanco(nome, lista_especifica, dados):
+    nome = {lista_especifica:dados}
+
+
+
 class Stm_Menus:
-
     def menu_funcionario(self):
-        #====================================================================================
-        Juan = G("Juan", "NUMERO", "123Mamaco^^")
-
-        # Banco de dados temporario
-        clientes = {"Joselina": {"Nome": "Joselina", "CPF": "NUMERO", "Historico": []}}
-        quartos = {}
-        alugueis = {}
-        funcionarios = [{"Juan": Juan}]
-        #=====================================================================================
-        # Informação do banco de dados
-        #=====================================================================================
+        funcionarios = f.get("funcionarios")
 
         while True:
-            mode = None                    #
-            for l in funcionarios:         # Ve se a lista tem um gerente
-                value = list(l.values())   #
-                for k in value:
-                    if type(k) == G:           # Se não tiver, entra no modo
-                        mode = "tem gerente"   # configuração de sistema.
+            mode = None                    # Ve se a lista tem um gerente
+            for l in funcionarios:         #
+                if type(l) == G:           # Se não tiver, entra no modo
+                    mode = "tem gerente"   # configuração de sistema.
 
             if mode != "tem gerente":
                 print("\n\nEntrando no modo configuração:")
                 Func_com_gerente = Stm_Menus.config(None,funcionarios)
-                funcionarios = Func_com_gerente
+                f["funcionarios"] = Func_com_gerente
             else: break
         #--------------------------------------------------------------
-        print("Menu Temporario 1:")
-        print("1) Login")
-        print("0) Sair")
+        while True: # Loop do menu de login
+            try:
+                print("\nMenu Temporario 1:")
+                print("1) Login")
+                print("0) Sair")
 
-        option = str(input(">"))
-        if option == "1":
-            login(funcionarios)
-        elif option == "0":
-            return
+                option = str(input(">"))
+                if option == "1":
+                    loginfo = login(funcionarios)
+                    if loginfo != None:
+                        Stm_Menus.menu_programa(loginfo)
+
+                elif option == "0":
+                    return None
+                else: raise ValueError
+
+            except ValueError:
+                print("Valor invalido\n\n")
 
     # ==============================================================================================
     def config(self, funcionarios):             # Gera uma conta gerente para se fazer login
@@ -53,4 +69,51 @@ class Stm_Menus:
 
     # ==============================================================================================
     def menu_programa(self):
-        pass
+        funcionarios = f.get("funcionarios")
+
+        while True:
+
+            print("\n\nMenu Temporario 2:")
+            print("1) Cadastro de cliente")
+            print("2) Processar aluguel")
+            print("3) Gerenciamento de clientes")
+            if type(self) == G:
+                print("4) Cadastro de funcionario")
+                print("5) Gerenciamento de funcionários")
+            print("0) Retornar")
+
+            option = str(input(">"))
+
+            if option == "1":
+                pass
+            # -------------------------------------------------------------------------------
+            elif option == "2":
+                pass
+            # -------------------------------------------------------------------------------
+            elif option == "3":
+                pass
+            # -------------------------------------------------------------------------------
+            elif option == "4" and type(self) == G:
+                func_temporario = G.cadastro_func(None,funcionarios,False)
+                if func_temporario != None:
+                    funcionarios = func_temporario
+
+                    print(funcionarios)
+                    f["funcionarios"] = funcionarios
+            # -------------------------------------------------------------------------------
+            elif option == "5" and type(self) == G:
+                print("Gerenciamento de funcionarios:")
+                print("1) Deletar")
+                print("2) Editar")
+
+                try:
+                    option = str(input(">"))
+
+                    #Continuar esta parte
+
+                except ValueError:
+                    return None
+            #-------------------------------------------------------------------------------
+            elif option == "0":
+                return None
+
