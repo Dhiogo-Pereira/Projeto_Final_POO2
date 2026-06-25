73ea8d9beb322e6e5e9007ea824513cb2d7bc852
Fox <dilvanajavier@gmail.com>
2026-06-25 16:02:24 -0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,3 +1,18 @@
 
-def login():
-    pass
+def login(lista_func):
+    print("\nLogin:")
+    try:
+        nome = str(input("Nome:").title())
+    except ValueError:
+        return None
+    try:
+        senha = str(input("Senha:"))
+    except ValueError:
+        return None
+
+    for l in lista_func:
+        if l.nome == nome and l.senha == senha:
+            return l
+
+    print("Nenhum cadastro encontrado.")
+    return None
